691802fad14ab2307df82d3c3f3c69a318a53fde
Chicco80 <chicco80lisa@gmail.com>
2022-05-16 11:50:30 +0200

    scaffholding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1 class="text-center">ndewnvciebvieqbviwfvir</h1>
+    <appHeader/>
+  </div>
+</template>
+
+<script>
+import appHeader from './components/appHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    appHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./styles/general.scss"
+</style>
